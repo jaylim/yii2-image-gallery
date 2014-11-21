7035cb3ca553b4f8ec62adbd836e1487580db24f
Jay Lim <jay@pentajeu.com>
2014-11-21 18:34:28 +0800

    auto load package

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+
+require dirname(__FILE__).'/models/Gallery.php';
+require dirname(__FILE__).'/models/GalleryPhoto.php';
+require dirname(__FILE__).'/GalleryAsset.php';
+require dirname(__FILE__).'/GalleryBehavior.php';
+require dirname(__FILE__).'/GalleryController.php';
+require dirname(__FILE__).'/GalleryManager.php';
